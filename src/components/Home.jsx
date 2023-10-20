253625c7a9fa72c06b9ca115c8113df8406957e8
MNazar <nazarmakarena@gmail.com>
2023-10-20 18:53:54 -0300

    Add home, navbar and assets

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import profile from '../assets/images/imgportfolio2.jpg'
+import profile from '../assets/images/profileph.jpeg'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
 
 function Home() {
     return (
@@ -8,10 +9,14 @@ function Home() {
                 <div className=' flex flex-col justify-center pl-24 h-full'>
                     <h1 className=' text-black font-Viberate text-text-huge'>Hello,</h1>
                     <p className=' text-black font-Viberate text-4xl font-medium'>I'm Makarena. A full stack developer, <br />and a creative mind.</p>
+                    <div className=' flex flex-row'>
+                    <a className=' max-w-fit' href="https://github.com/MNazar8">< BsGithub className=' mt-6 h-8 w-auto' /></a>
+                    <a href="https://www.linkedin.com/in/makarena-nazar-32948b120/"><BsLinkedin className=' mt-6 h-8 w-auto ml-6'/></a>
+                    </div>
                 </div>
             </div>
             <div className='w-3/12'>
-                <img src={profile} className=' w-full pr-2 h-auto pt-20 rounded-md' alt="" />
+                <img src={profile} className=' w-full pr-2 pt-20' alt="" />
             </div>
         </div>
     )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,10 @@ export default function NavBar() {
     const [navbar, setNavbar] = useState(false)
     const navItems = [
         {
+            label: 'HOME',
+            page: 'home',
+        },
+        {
             label: 'PROJECTS',
             page: 'projects',
         },
@@ -21,7 +25,7 @@ export default function NavBar() {
     ]
 
     return (
-        <header className=' w-full fixed bg-white'>
+        <header className="w-full fixed bg-white z-50 ">
             <div className=' flex flex-row justify-between items-center px-12 '>
                 <div>
                     <Link
@@ -37,20 +41,24 @@ export default function NavBar() {
                         <img src={logo} alt="" className=' h-16 w-auto cursor-pointer' />
                     </Link>
                 </div>
-                <div className=''>
+                <div className=' flex'>
                     {navItems.map((item, index) => {
                         return (
                             <Link
                                 key={index}
                                 to={item.page}
-                                className=' ml-8 font-Montserrat tracking-widest cursor-pointer hover:font-bold'
+                                className={`ml-8 font-Montserrat tracking-widest cursor-pointer relative group ${
+                                    item.page === 'active' ? 'font-bold' : 'hover:font-bold'
+                                  }`}
                                 activeClass='active'
                                 spy={true}
                                 smooth={true}
                                 offset={-63}
                                 duration={500}
                                 onClick={() => setNavbar(!navbar)}
-                            >{item.label}</Link>
+                            >{item.label}
+                            <div className="absolute w-full h-0.5 bg-black bottom-0 left-0 origin-left transform scale-x-0 transition-transform group-hover:scale-x-100"></div>
+                            </Link>
                         )
                     })}
                 </div>
